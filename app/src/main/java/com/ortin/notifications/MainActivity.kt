a7980b788da825376f790116b23b4590f488bbb4
ImmortalIdiot <max.krasulya@yandex.ru>
2025-10-26 19:48:27 +0300

    feat: use login in MainActivity

--- app/src/main/java/com/ortin/notifications/MainActivity.kt
+++ app/src/main/java/com/ortin/notifications/MainActivity.kt
@@ -10,43 +10,41 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ortin.notifications.presentation.auth.AuthorizationViewModel
 import com.ortin.notifications.presentation.auth.LoginScreen
 import com.ortin.notifications.ui.theme.NotificationsTheme
 import kotlinx.coroutines.delay
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
+
+    private val loginViewModel: AuthorizationViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var login by remember { mutableStateOf("") }
-            var password by remember { mutableStateOf("") }
-            var loading by remember { mutableStateOf(false) }
-            var error by remember { mutableStateOf<String?>(null) }
-
-            LaunchedEffect(loading) {
-                if (loading) {
-                    delay(2000)
-                    loading = false
-                    error = "Пароль неверный"
-                }
-            }
+            val uiState by loginViewModel.uiState.collectAsState()
 
             NotificationsTheme {
+
+                // TODO: replace with navigation in the future
                 LoginScreen(
-                    login = login,
-                    onLoginChange = { login = it; error = null },
-                    password = password,
-                    onPasswordChange = { password = it; error = null },
-                    isLoading = loading,
-                    errorText = error,
-                    onLoginClick = { loading = true },
+                    login = uiState.id,
+                    onLoginChange = { loginViewModel.changeId(it) },
+                    password = uiState.password,
+                    onPasswordChange = { loginViewModel.changePassword(it) },
+                    isLoading = uiState.loading,
+                    errorText = uiState.errorText,
+                    onLoginClick = loginViewModel::login,
                     onErrorDismiss = {}
                 )
             }
