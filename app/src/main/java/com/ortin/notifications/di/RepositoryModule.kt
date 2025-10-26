e4680a9dda0a041ec2ede6444b1b36b22a6b9e29
ImmortalIdiot <max.krasulya@yandex.ru>
2025-10-26 19:30:17 +0300

    feat: add user preferences

--- app/src/main/java/com/ortin/notifications/di/RepositoryModule.kt
+++ app/src/main/java/com/ortin/notifications/di/RepositoryModule.kt
@@ -1,9 +1,11 @@
 package com.ortin.notifications.di
 
+import com.ortin.notifications.data.datastore.UserPreferences
 import com.ortin.notifications.data.impl.AuthorizationRepositoryImpl
 import com.ortin.notifications.domain.repository.AuthorizationRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
+    single { UserPreferences(get()) }
     single<AuthorizationRepository> { AuthorizationRepositoryImpl(get()) }
 }
