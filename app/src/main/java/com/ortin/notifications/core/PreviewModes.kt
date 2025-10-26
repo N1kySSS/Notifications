98b3f666d2cae5c8271f57389caed9ce2d911210
ImmortalIdiot <max.krasulya@yandex.ru>
2025-10-26 17:35:41 +0300

    feat: add light and night preview modes

---
+++ app/src/main/java/com/ortin/notifications/core/PreviewModes.kt
@@ -0,0 +1,16 @@
+package com.ortin.notifications.core
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+annotation class LightModePreview
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+annotation class NightModePreview
