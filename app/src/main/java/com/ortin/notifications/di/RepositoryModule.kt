7b251a6b6aa48602ac2967ba68b7cf2fad75119b
ImmortalIdiot <max.krasulya@yandex.ru>
2025-10-27 20:27:46 +0300

    di: replace real implementation with mock

--- app/src/main/java/com/ortin/notifications/di/RepositoryModule.kt
+++ app/src/main/java/com/ortin/notifications/di/RepositoryModule.kt
@@ -2,7 +2,7 @@ package com.ortin.notifications.di
 
 import com.ortin.notifications.data.datastore.UserPreferences
 import com.ortin.notifications.data.impl.AuthorizationRepositoryImpl
-import com.ortin.notifications.data.impl.NotificationRepositoryImpl
+import com.ortin.notifications.data.mock.MockNotificationRepository
 import com.ortin.notifications.domain.repository.AuthorizationRepository
 import com.ortin.notifications.domain.repository.NotificationRepository
 import org.koin.dsl.module
@@ -10,5 +10,5 @@ import org.koin.dsl.module
 val repositoryModule = module {
     single { UserPreferences(get()) }
     single<AuthorizationRepository> { AuthorizationRepositoryImpl(get(), get()) }
-    single<NotificationRepository> { NotificationRepositoryImpl(get(), get()) }
+    single<NotificationRepository> { /* TODO: replace with real implementation before merging */ MockNotificationRepository() }
 }
