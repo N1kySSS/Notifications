b99eb78861ac3277edad95dbd41cacb36f787488
ImmortalIdiot <max.krasulya@yandex.ru>
2025-10-27 21:49:40 +0300

    chore: use main screen in main activity for demo

--- app/src/main/java/com/ortin/notifications/MainActivity.kt
+++ app/src/main/java/com/ortin/notifications/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,36 +21,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
 import com.ortin.notifications.presentation.auth.AuthorizationViewModel
 import com.ortin.notifications.presentation.auth.LoginScreen
+import com.ortin.notifications.presentation.main.MainScreen
+import com.ortin.notifications.presentation.main.MainScreenViewModel
+import com.ortin.notifications.ui.components.cards.NotificationCard
 import com.ortin.notifications.ui.theme.NotificationsTheme
+import com.ortin.notifications.ui.utils.Constants
 import kotlinx.coroutines.delay
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
 
     private val loginViewModel: AuthorizationViewModel by viewModel()
+    private val notificationsViewModel: MainScreenViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val uiState by loginViewModel.uiState.collectAsState()
-
+            val uiState by notificationsViewModel.uiState.collectAsState()
             NotificationsTheme {
-
-                // TODO: replace with navigation in the future
-                LoginScreen(
-                    login = uiState.id,
-                    onLoginChange = { loginViewModel.changeId(it) },
-                    password = uiState.password,
-                    onPasswordChange = { loginViewModel.changePassword(it) },
+                MainScreen(
+                    items = uiState.notifications,
                     isLoading = uiState.loading,
+                    isRefreshing = uiState.isRefreshing,
                     errorText = uiState.errorText,
-                    onLoginClick = loginViewModel::login,
+                    onRefresh = notificationsViewModel::loadData,
                     onErrorDismiss = {}
                 )
             }
